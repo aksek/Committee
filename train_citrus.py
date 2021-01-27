81fe3e085ac798486e511befa2f4a3f4c2750fbb
aksek <kosekaniela@gmail.com>
2021-01-27 19:26:48 +0100

    output all test results

--- train_citrus.py
+++ train_citrus.py
@@ -32,34 +32,42 @@ df = pd.read_csv("citrus.csv")
 X = df.drop("name", axis=1)
 y = df["name"].values
 
-accuracy = []
-precision = []
-recall = []
-f_score = []
+# initialize classifiers
+clf1 = DecisionTree()
+clf2 = KNeighborsClassifier(n_neighbors=7)
+clf3 = SVC(kernel='rbf', probability=True)
+clf4 = GaussianNB()
 
-for i in range(25):
-    # split into training and testing sets
-    X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2)
+test_cases = [[clf1], [clf2], [clf3], [clf4], [clf1, clf2, clf3, clf4], [clf2, clf3, clf4]]
 
-    # initialize classifiers
-    clf1 = DecisionTree()
-    clf2 = KNeighborsClassifier(n_neighbors=7)
-    clf3 = SVC(kernel='rbf', probability=True)
-    clf4 = GaussianNB()
+test_number = 1
+for classifiers in test_cases:
 
-    # initialize and fit the voting classifier
-    eclf = cmt.CommitteeClassifier([clf1])
-    eclf = eclf.fit(X_train, y_train)
+    accuracy = []
+    precision = []
+    recall = []
+    f_score = []
 
-    # test the result
-    acc, prec, rec, f = calculate_metrics(eclf, X_test, y_test)
-    accuracy.append(acc)
-    precision.append(prec)
-    recall.append(rec)
-    f_score.append(f)
-    print(str(i * 4) + "%")
+    for i in range(25):
+        # split into training and testing sets
+        X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2)
 
-print("Accuracy: " + str(mean(accuracy)))
-print("Precision: " + str(mean(precision)))
-print("Recall: " + str(mean(recall)))
-print("F_score: " + str(mean(f_score)))
+        # initialize and fit the voting classifier
+        eclf = cmt.CommitteeClassifier(classifiers)
+        eclf = eclf.fit(X_train, y_train)
+
+        # test the result
+        acc, prec, rec, f = calculate_metrics(eclf, X_test, y_test)
+        accuracy.append(acc)
+        precision.append(prec)
+        recall.append(rec)
+        f_score.append(f)
+        print(str((i + 1) * 4) + "%")
+
+    print(test_number)
+    print("Accuracy: " + str(mean(accuracy)))
+    print("Precision: " + str(mean(precision)))
+    print("Recall: " + str(mean(recall)))
+    print("F_score: " + str(mean(f_score)))
+    print()
+    test_number += 1
