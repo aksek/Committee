6c2e6ec7e72c1b981bdcccbb1064c90d5eab5c58
aksek <kosekaniela@gmail.com>
2021-01-27 12:25:10 +0100

    updated the training script for tennis to support new encoding

--- train_tennis.py
+++ train_tennis.py
@@ -1,32 +1,17 @@
 # Aniela Kosek
+import random
 from statistics import mean
 
 import pandas as pd
-import numpy as np
-from sklearn.metrics import confusion_matrix, accuracy_score, precision_score, recall_score, f1_score
 from sklearn.model_selection import train_test_split
 from sklearn.naive_bayes import GaussianNB
 from sklearn.neighbors import KNeighborsClassifier
-from sklearn.preprocessing import OneHotEncoder
 from sklearn.svm import SVC
-from sklearn.tree import DecisionTreeClassifier
 
-import committee as cmt
+from committee import CommitteeClassifier
 from decisionTree import DecisionTree
-
-
-def calculate_metrics(model, X_test, y_test):
-    pred = model.predict(X_test)
-    cm = confusion_matrix(y_test, pred)
-    acc = accuracy_score(y_test, pred)
-    precision = precision_score(y_test, pred, pos_label=1, zero_division=0)
-    recall = recall_score(y_test, pred, pos_label=1)
-    f_score = f1_score(y_test, pred, pos_label=1)
-    # print('Confusion matrix:\n', cm)
-    # print('Accuracy: {}\nPrecision: {}\nRecall: {}\nF1_score: {}'.format(
-    #     acc, precision, recall, f_score))
-    return acc, precision, recall, f_score
-
+from utility import calculate_metrics
+from utility import encode
 
 # load data
 df = pd.read_csv("play_tennis.csv")
@@ -34,19 +19,10 @@ df = pd.read_csv("play_tennis.csv")
 # drop irrelevant columns
 df = df.drop("day", axis=1)
 
-# Encode categorical columns
-categoricals = list(df.select_dtypes(include=['O']).columns)
-encoder = OneHotEncoder(sparse=False)
-encoded = encoder.fit_transform(df[categoricals])
-
-# create a data frame with the encoded data and append it
-train_ohe = pd.DataFrame(encoded, columns=np.hstack(encoder.categories_))
-df = pd.concat((df, train_ohe), axis=1).drop(categoricals, axis=1)
-
 # split columns and drop irrelevant columns
-X = df.drop("No", axis=1)
-X = X.drop("Yes", axis=1)
-y = df["Yes"].values
+X = df.drop("play", axis=1)
+y = df["play"].values
+encoded_X = encode(X)
 
 accuracy = []
 precision = []
@@ -55,20 +31,22 @@ f_score = []
 
 for i in range(25):
     # split into training and testing sets
-    X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.4)
+    rand = random.randrange(100000)
+    X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=rand)
+    encoded_X_train, encoded_X_test = train_test_split(encoded_X, test_size=0.2, random_state=rand)
 
     # initialize classifiers
     clf1 = DecisionTree()
     clf2 = KNeighborsClassifier(n_neighbors=2)
-    # clf3 = SVC(kernel='rbf', probability=True)
-    # clf4 = GaussianNB()
+    clf3 = SVC(kernel='rbf', probability=True)
+    clf4 = GaussianNB()
 
     # initialize and fit the voting classifier
-    eclf = cmt.CommitteeClassifier([clf1, clf2])
-    eclf = eclf.fit(X_train, y_train)
+    eclf = CommitteeClassifier([clf2])
+    eclf = eclf.fit(X_train, y_train, encoded_X=encoded_X_train)
 
     # test the result
-    acc, prec, rec, f = calculate_metrics(eclf, X_test, y_test)
+    acc, prec, rec, f = calculate_metrics(eclf, X_test, y_test, encoded_X=encoded_X_test)
     accuracy.append(acc)
     precision.append(prec)
     recall.append(rec)
---
+++ utility.py
@@ -0,0 +1,34 @@
+# Aniela Kosek
+from sklearn.metrics import confusion_matrix, accuracy_score, precision_score, recall_score, f1_score
+from sklearn.preprocessing import OneHotEncoder
+import pandas as pd
+import numpy as np
+
+
+def encode(df):
+    X = df
+    # Encode categorical columns
+    categoricals = list(X.select_dtypes(include=['O']).columns)
+    encoder = OneHotEncoder(sparse=False)
+    encoded = encoder.fit_transform(X[categoricals])
+
+    # create a data frame with the encoded data and append it
+    if not (encoded.size == 0):
+        train_ohe = pd.DataFrame(encoded, columns=np.hstack(encoder.categories_))
+        X = pd.concat((X, train_ohe), axis=1).drop(categoricals, axis=1)
+        return X
+    else:
+        return df
+
+
+def calculate_metrics(model, X_test, y_test, *, encoded_X=None):
+    pred = model.predict(X_test, X_encoded=encoded_X)
+    cm = confusion_matrix(y_test, pred)
+    acc = accuracy_score(y_test, pred)
+    precision = precision_score(y_test, pred, zero_division=0, average='macro')
+    recall = recall_score(y_test, pred, zero_division=0, average='macro')
+    f_score = f1_score(y_test, pred, zero_division=0, average='macro')
+    # print('Confusion matrix:\n', cm)
+    # print('Accuracy: {}\nPrecision: {}\nRecall: {}\nF1_score: {}'.format(
+    #     acc, precision, recall, f_score))
+    return acc, precision, recall, f_score
